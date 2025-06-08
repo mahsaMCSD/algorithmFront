270199abfa6c0cde09782ffe98c205bb8eae62ca
mahsa <mahsaashrafi71@gmail.com>
2025-06-08 23:02:18 +0330

    diagonal data structure using Map

--- js/chessboard.js
+++ js/chessboard.js
@@ -3,21 +3,30 @@ export default {
   highlight,
 };
 
-var tiles=[];
+var diagonals = [];
+var highlighted=[] ;
+var tileDiagonals = new Map();
 
 // ****************************
 
 function draw(boardEl) {
+  //setting my diagonals data structure
+  for (let i = 0; i < 30; i++) {
+    diagonals.push([]);
+  }
+
   for (let i = 0; i < 8; i++) {
     let rowEl = document.createElement("div");
-	let rowTiles=[]
-	tiles.push(rowTiles);
     for (let j = 0; j < 8; j++) {
       let tileEl = document.createElement("div");
-      tileEl.dataset.row = i;
-      tileEl.dataset.col = j;
+
       rowEl.appendChild(tileEl);
-	  rowTiles.push(tileEl);
+      let majorDiagonal = diagonals[7 - (i - j)];
+      let minorDiagonal = diagonals[15 + (i + j)];
+
+      majorDiagonal.push(tileEl);
+      minorDiagonal.push(tileEl);
+      tileDiagonals.set(tileEl, [majorDiagonal, minorDiagonal]);
     }
     boardEl.appendChild(rowEl);
   }
@@ -25,34 +34,18 @@ function draw(boardEl) {
 
 function highlight(tileEl) {
   //clear currently highlighted
-  for (let row of tiles) {
-	for (let el of row) {
-	  el.classList.remove("highlighted");
-	}
-  }
+  for (let diagonal of highlighted) {
+      for (let el of diagonal) {
+		el.classList.remove("highlighted");
+      }
+    }
   if (tileEl) {
-    let tileRowIdx = +tileEl.dataset.row;
-    let tileColIdx = +tileEl.dataset.col;
+     highlighted = tileDiagonals.get(tileEl);
 
-    //highlight in up-left direction
-    for (let i = tileRowIdx, j = tileColIdx; i >= 0 && j >= 0; i--, j--) {
-	  let el=tiles[i][j];
-      el.classList.add("highlighted");
-    }
-    //highlight in up-right direction
-    for (let i = tileRowIdx, j = tileColIdx; i >= 0 && j < 8; i--, j++) {
-	  let el=tiles[i][j];
-      el.classList.add("highlighted");
-    }
-    //highlight in down-left direction
-    for (let i = tileRowIdx, j = tileColIdx; i < 8 && j >= 0; i++, j--) {
-	  let el=tiles[i][j];
-      el.classList.add("highlighted");
-    }
-    //highlight in down-right direction
-    for (let i = tileRowIdx, j = tileColIdx; i < 8 && j < 8; i++, j++) {
-	  let el=tiles[i][j];
-      el.classList.add("highlighted");
+    for (let diagonal of highlighted) {
+      for (let el of diagonal) {
+		el.classList.add("highlighted");
+      }
     }
   }
 }
