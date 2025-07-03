7deef78bfeb5b01d74e4cc006820aff5a4d0890d
mahsa <mahsaashrafi71@gmail.com>
2025-07-03 20:41:14 +0330

    reachable key function

--- js/dialer.js
+++ js/dialer.js
@@ -1,39 +1,63 @@
 export default {
-	reachableKeys,
-	countPaths,
-	listAcyclicPaths
+  reachableKeys,
+  countPaths,
+  listAcyclicPaths,
 };
-
 
 // ****************************
 
+var dialed = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [, 0],
+];
+
 function reachableKeys(startingDigit) {
-	// TODO: return which digits a Knight's move
-	// can hop to from a given starting digit/key
-	//
-	// e.g. 3 -> [ 4, 8 ]
-	//      4 -> [ 3, 9, 0 ]
-	//      5 -> []
-	return [];
+  var reachable = [];
+  for (let [rowIdx, row] of dialed.entries()) {
+    let colIdx = row.indexOf(startingDigit);
+    if (colIdx !== -1) {
+      for (let rowMove of [-2, -1, 1, 2]) {
+        for (let colMove of [-2, -1, 1, 2]) {
+          if (Math.abs(rowMove) !== Math.abs(colMove)) {
+            let newRowIdx = rowIdx + rowMove;
+            let newColIdx = colIdx + colMove;
+            if (
+              newRowIdx >= 0 &&
+              newRowIdx < dialed.length &&
+              newColIdx >= 0 &&
+              newColIdx < dialed[newRowIdx].length &&
+              dialed[newRowIdx][newColIdx] !== undefined
+            ) {
+              reachable.push(dialed[newRowIdx][newColIdx]);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return reachable;
 }
 
-function countPaths(startingDigit,hopCount) {
-	// TODO: given the digit/key to start from and
-	// the number of hops to take, return a count
-	// of all the possible paths that could be
-	// traversed
-	return 0;
+function countPaths(startingDigit, hopCount) {
+  // TODO: given the digit/key to start from and
+  // the number of hops to take, return a count
+  // of all the possible paths that could be
+  // traversed
+  return 0;
 }
 
 function listAcyclicPaths(startingDigit) {
-	// TODO: given the digit/key to start from,
-	// return a list of the distinct acyclic
-	// paths that are possible to traverse
-	//
-	// e.g. [
-	//   [4, 3, 8, 1, 6, 7, 2, 9],
-	//   [4, 3, 8, 1, 6, 0],
-	//   ...
-	// ]
-	return [];
+  // TODO: given the digit/key to start from,
+  // return a list of the distinct acyclic
+  // paths that are possible to traverse
+  //
+  // e.g. [
+  //   [4, 3, 8, 1, 6, 7, 2, 9],
+  //   [4, 3, 8, 1, 6, 0],
+  //   ...
+  // ]
+  return [];
 }
