aaf7b864db785419d3d0e4b10a97896fd569b230
mahsa <mahsaashrafi71@gmail.com>
2025-07-05 19:08:12 +0330

    dynamic programming

--- js/dialer.js
+++ js/dialer.js
@@ -4,8 +4,6 @@ export default {
   listAcyclicPaths,
 };
 
-countPaths=memoize(countPaths);
-
 // ****************************
 
 var nearbykeys = [
@@ -29,11 +27,18 @@ function countPaths(startingDigit, hopCount) {
   if (hopCount === 0) {
     return 1;
   }
-  var pathCount = 0;
-  for (let digit of reachableKeys(startingDigit)) {
-    pathCount += countPaths(digit, hopCount - 1);
+  //dynamic programming approach
+  var priorPathCounts = Array(10).fill(1);
+  for (let hops = 0; hops < hopCount; hops++) {
+    let pathCounts = Array(10).fill(0);
+    for (let digit = 0; digit <= 9; digit++) {
+      for (let n of nearbykeys[digit]) {
+        pathCounts[digit] += priorPathCounts[n];
+      }
+    }
+    priorPathCounts = pathCounts;
   }
-  return pathCount;
+  return priorPathCounts[startingDigit];
 }
 
 function listAcyclicPaths(startingDigit) {
@@ -65,23 +70,4 @@ function followPaths(path, paths) {
   if (!pathForwardPath) {
     paths.push(path);
   }
-}
-
-function memoize(fn) {
-  var cache = {};
-  // Create a cache object to store results of previous calls
-  // The cache will use a string key based on the function arguments
-  // to store the results of the function calls.
-  // This allows us to avoid recalculating results for the same arguments.  
-  // The key is constructed as a string in the format "start:length"
-  // where 'start' is the starting digit and 'length' is the hop count. 
-  return function memoized(start, length) {
-    if (!cache[`${start}:${length}`]) {
-      // If the result is not in the cache, call the original function
-      // and store the result in the cache. 
-      
-      cache[`${start}:${length}`] = fn(start, length);
-    }
-    return cache[`${start}:${length}`];
-  };
 }
